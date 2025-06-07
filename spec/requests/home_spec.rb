4f5394a54b235f73206c6850b38dc9ad464825d1
Ian Fleeton <ianfleeton@gmail.com>
2025-06-07 22:51:05 +0100

    Create account

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,15 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "Home", type: :request do
-  let(:user) { FactoryBot.create(:user) }
-  before { login(user) }
-
-  def login(user)
-    session = Session.new(user:)
-    allow(Current).to receive(:session).and_return(session)
-    allow(Current).to receive(:user).and_return(user)
-  end
-
   describe "GET /" do
     it "returns HTTP success" do
       get "/"
---
+++ spec/system/create_account_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "Create Account", type: :system do
+  scenario "Create an account happy path" do
+    visit "/"
+    click_link "Create account"
+    expect(page).to be_axe_clean
+    expect(page).to have_selector("h1", text: "Create account")
+    fill_in "Email address", with: "newuser@example.com"
+    fill_in "Password", with: "password"
+    click_button "Create account"
+
+    expect(page).to have_text("Account created")
+    expect(page).to be_axe_clean
+    expect(User.count).to eq(1)
+    user = User.first
+    expect(user.email_address).to eq("newuser@example.com")
+  end
+end
